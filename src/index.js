a75fec449822f56fd3e34afb537f74277037a481
cryptowind <engr.bruce.wayne00@gmail.com>
2018-03-12 14:13:38 +0600

    Switching Component

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-class App extends React.Component {
 
+function Message(props){
+  if (props.value) {
+    return <h1>this is the first message</h1>
+  }
+    return <h2>this is the second message</h2>
+}
+class Btn extends React.Component {
    constructor(props){
        super(props);
-       this.state={ count: 1}
+       this.state = {value: true}
    }
+   handleClick = () => {
+     this.setState({
+value : !this.state.value
+});
+}
+render(){
+return(
+  <div>
+   <button onClick={this.handleClick}>Change The Message</button>
+   <Message value={this.state.value} />
+</div>
 
-  onclick(type){
-      this.setState(prevState => {
-         return {count: type == 'add' ? prevState.count + 1: prevState.count - 1}
-      });
-  }
-
-   render() {
-    return (
-      <div>
-        Count: {this.state.count}
-        <br/>
-        <div style={{marginTop: '100px'}}/>
-        <input type='button' onClick={this.onclick.bind(this, 'add')} value='Inc'/>
-        <input type='button' onClick={this.onclick.bind(this, 'sub')} value='Dec'/>
-       </div>
-     )
-   }
+);
+}
 }
 
 ReactDOM.render(
-  <App />,
+  <Btn />,
   document.getElementById('root')
 );
