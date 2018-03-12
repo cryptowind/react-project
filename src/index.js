64930a47ac6b334c6072d2c8b522a55aad7e0033
cryptowind <engr.bruce.wayne00@gmail.com>
2018-03-12 15:42:29 +0600

    List Creation

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Message(props){
-  if (props.value) {
-    return <h1>this is the first message</h1>
-  }
-    return <h2>this is the second message</h2>
-}
-class Btn extends React.Component {
-   constructor(props){
-       super(props);
-       this.state = {value: true}
-   }
-   handleClick = () => {
-     this.setState({
-value : !this.state.value
-});
-}
-render(){
-return(
-  <div>
-   <button onClick={this.handleClick}>Change The Message</button>
-   <Message value={this.state.value} />
-</div>
-
-);
-}
-}
-
-ReactDOM.render(
-  <Btn />,
-  document.getElementById('root')
-);
+const cartoons = ['tom', 'penny', 'grey','tom']
+const toons = cartoons.map((cartoons,index)=><li key={index}>{cartoons}</li>)
+  ReactDOM.render(
+    <ul>{toons}</ul>,
+    document.getElementById('root')
+  );
